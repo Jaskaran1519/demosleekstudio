a48867bc715f8324220e28243e66fb91f8be3a2a
Jaskaran1519 <jaskaran1519@gmail.com>
2025-04-09 21:28:25 +0530

    sfkns

--- components/Hero/BestSellers.tsx
+++ components/Hero/BestSellers.tsx
@@ -3,31 +3,35 @@ import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious
 import { getFeaturedProducts } from "@/actions/products"
 import ProductCard from "@/components/Others/ProductCard"
 import { Suspense } from "react"
+import { Product } from "@/types"
 
 export const BestSellers = async () => {
   const { products } = await getFeaturedProducts(8);
 
   return (
     <div className="w-[90%] mx-auto mt-6 md:mt-10">
-      <h1 className="text-4xl my-3 ">Our BestSellers</h1>
+      <h1 className="text-4xl my-3">Our BestSellers</h1>
       <Suspense fallback={<div>Loading...</div>}>
         <Carousel
           opts={{
             align: "start",
+            loop: true,
+            skipSnaps: false,
+            dragFree: true,
           }}
-          className="w-full "
+          className="w-full"
         >
-          <CarouselContent>
-            {products.map((product) => (
-              <CarouselItem key={product.id} className="basis-1/1 md:basis-1/2 lg:basis-1/3 xl:basis-1/4">
+          <CarouselContent className="-ml-2 md:-ml-4">
+            {products.map((product: Product) => (
+              <CarouselItem key={product.id} className="pl-2 md:pl-4 basis-1/2 sm:basis-1/3 md:basis-1/3 lg:basis-1/4">
                 <div className="p-1">
                   <ProductCard product={product} />
                 </div>
               </CarouselItem>
             ))}
           </CarouselContent>
-          <CarouselPrevious className="left-0" />
-          <CarouselNext />
+          <CarouselPrevious className="hidden sm:flex" />
+          <CarouselNext className="hidden sm:flex" />
         </Carousel>
       </Suspense>
     </div>
