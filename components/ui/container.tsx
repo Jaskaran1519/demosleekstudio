69c0e2c1db1ae4d1fe11e0e7cd23863ec656f222
Jaskaran1519 <jaskaran1519@gmail.com>
2025-04-29 13:53:15 +0530

    container px

--- components/ui/container.tsx
+++ components/ui/container.tsx
@@ -5,7 +5,7 @@ interface ContainerProps extends React.HTMLAttributes<HTMLDivElement> {}
 export const Container = ({ className, children, ...props }: ContainerProps) => {
   return (
     <div 
-      className={cn("mx-auto w-full max-w-[1800px] min-h-screen  lg:px-8 xl:px-10 mt-5", className)}
+      className={cn("mx-auto w-full max-w-[1800px] min-h-screen px-4 md:px-6  lg:px-8 xl:px-10 mt-5", className)}
       {...props}
     >
       {children}
